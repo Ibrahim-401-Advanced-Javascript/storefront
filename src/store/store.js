c882cac60e46bc46da76a3d803ba5073cadb5e50
Cas Ibrahim <qasiraibrahim@gmail.com>
2020-09-30 23:10:54 -0700

    added redux-thunk and made small changes to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,5 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import { composeWithDevTools } from 'redux-devtools-extension';
 
@@ -12,7 +13,7 @@ let reducers = combineReducers({
 });
 
 const store = () => {
-  return createStore(reducers, composeWithDevTools());
+  return createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
 };
 
 export default store();
